ab3d52a7eb75aa7e68257d2cbd24c27769d78e4c
jdgomes <jgomes@apterainc.com>
2016-11-06 09:14:14 -0500

    Ignore tests

--- effectively.tests/BreakoutClass/The_game.cs
+++ effectively.tests/BreakoutClass/The_game.cs
@@ -19,9 +19,10 @@ namespace effectively.tests.BreakoutClass
         public class GivenThePlayerHasJustGottenOutOfThePenaltyBox
         {
             [Test]
+            [Ignore("This needs to be implemented!")]
             public void ThenThePlayersPlaceIsOnlyAdvancedByOneLessThanTheCurrentRoll()
             {
-
+                Assert.Fail("Not implemented!");
             }
         }
     }
--- effectively.tests/StaticCling/The_database.cs
+++ effectively.tests/StaticCling/The_database.cs
@@ -8,6 +8,8 @@
         public class when_querying {
 
             [Test]
+            [Ignore("This needs to be implemented!")]
+
             public void logs_the_sql() {
                 Assert.Fail("Not implemented!");
             }
