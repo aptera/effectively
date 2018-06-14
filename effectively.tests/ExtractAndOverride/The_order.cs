bdbb4015a54075fa970baa3e5cf2aac4866905e2
Jon Fazzaro <jfazzaro@apterainc.com>
2018-06-14 12:48:12 -0400

    kata: extract and override

--- effectively.tests/ExtractAndOverride/The_order.cs
+++ effectively.tests/ExtractAndOverride/The_order.cs
@@ -1,15 +1,63 @@
 ﻿namespace effectively.tests.ExtractAndOverride
 {
+	using effectively.ExtractAndOverride;
 	using NUnit.Framework;
 
 	[TestFixture]
-    public class The_order
-    {
-            //Given the current user is valid
-            //  Given the order amount is negative
-            //  The order cannot be saved
+	public class The_order
+	{
+		[TestFixture]
+		public class GivenTheCurrentUserIsValid
+		{
 
-            //  Given the order amount is 0 or positive
-            //  The order can be saved 
-    }
+			[Test]
+			public void GivenTheOrderAmountIsNegative_TheOrderCannotBeSaved()
+			{
+				Assert.IsFalse(OrderWithAmountIsValid(-1));
+			}
+
+			[Test]
+			public void GivenTheOrderAmountIsZero_TheOrderCanBeSaved()
+			{
+				Assert.IsTrue(OrderWithAmountIsValid(0));
+			}
+			
+			[Test]
+			public void GivenTheOrderAmountIsGreaterThanZero_TheOrderCanBeSaved()
+			{
+				Assert.IsTrue(OrderWithAmountIsValid(1));
+			}
+
+			private bool OrderWithAmountIsValid(int amount)
+			{
+				var order = new Order { Amount = amount };
+				var os = new TestableOrderService { UserIsValid = true };
+
+				return os.Add(order);
+			}
+		}
+
+		[TestFixture]
+		public class GivenTheCurrentUserIsInvalid
+		{
+			[Test]
+			public void TheOrderCannotBeSaved()
+			{
+				var order = new Order { Amount = 1 };
+				var os = new TestableOrderService { UserIsValid = false };
+				Assert.IsFalse(os.Add(order));
+			}
+		}
+
+		class TestableOrderService 
+			: OrderService
+		{
+			public bool UserIsValid { get; set; }
+
+			protected override UserService GetUserService()
+			{
+				return new UserService { isValidUser = this.UserIsValid };
+			}
+		}
+	}
 }
--- effectively/ExtractAndOverride/OrderService.cs
+++ effectively/ExtractAndOverride/OrderService.cs
@@ -2,19 +2,26 @@
 {
     public class OrderService
     {
-        public bool Add(Order order)
-        {
-            var userService = new UserService();
-            if (userService.isValidUser)//Can the current user save an order
-            {
-                //save order
-                return true;
-            }
-            else {
-                //dont add send an exception and log it
-                return false;
-            }
-        }
-    }
+	   public bool Add(Order order)
+		{
+			UserService userService = GetUserService();
+
+			if (userService.isValidUser &&  order.Amount >= 0)//Can the current user save an order
+			{
+				//save order
+				return true;
+			}
+			else
+			{
+				//dont add send an exception and log it
+				return false;
+			}
+		}
+
+		protected virtual UserService GetUserService()
+		{
+			return new UserService();
+		}
+	}
 }
     
